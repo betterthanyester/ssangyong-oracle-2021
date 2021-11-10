d766e3d5e7ab473f681192f20f4666a252b6d65f
rlgus3311 <rlgus3311@naver.com>
2021-11-10 11:53:02 +0900

    Update ex03_select.sql

--- ex03_select.sql
+++ ex03_select.sql
@@ -56,4 +56,62 @@ WHERE 조건
 
 
 select name
-from tblCountry;
+from tblCountry; 
+
+/*
+SQL이 구현한 Projection 연산
+행의 개수 = 원본과 동일한 14개
+컬럼의 개수 = 1개 = select에 적어놓은 name
+
+*/
+
+
+select capital from tblCountry;
+
+
+select population from tblCountry;
+
+
+
+
+desc book; -- 데이터 구조 파악
+
+select bookname from book; -- 책이름 컬럼만 가져오기
+
+/*
+select      -- from절을 먼저 적으면, select에 인텔리센스가 뜬다!!! 그래서 from절부터 먼저 쓰기도 한다. 
+from book;
+*/
+
+
+select name from tblCountry;
+select name,capital from tblCountry;
+
+select name, capital, population, continent, area
+from tblCountry;   -- * 과 같은 의미여도, 가독성이 높아서 많이 씀
+
+
+select * from tblcountry;  -- *(와일드카드) : 다 가져옴
+
+-- 식별자가 틀린 경우
+-- ORA-00904: "NAME2": invalid identifier
+select name2 from tblCountry;
+
+-- 테이블명이 틀린 경우
+-- ORA-00942: table or view does not exist
+select name from tblCountry2;
+
+
+-- 컬럼 리스트의 순서는 원본 테이블의 컬럼 순서와 무관하다. (개발자가 순서를 재정의할 수 있음)
+select name, capital from tblCountry;
+select capital, name from tblCountry; 
+
+-- 동일한 컬럼을 여러번 가져와도 된다.
+select name, name from tblCountry;
+
+-- 다만, 보통 가공 처리를 해서 가져온다.
+-- SQL에서 문자열 더할 때는 '||' (OR)을 쓴다.
+select name, name || '@' from tblCountry;
+
+
+
